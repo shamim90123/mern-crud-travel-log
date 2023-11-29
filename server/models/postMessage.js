7ae3b1a6bfcf9b9da8f759c813f0631f5c737d95
Md Shamim Reza <opt.shamim@gmail.com>
2023-11-30 00:03:20 +0600

    first commit

---
+++ server/models/postMessage.js
@@ -0,0 +1,21 @@
+import mangoose from 'mongoose';
+
+const postSchema = mangoose.Schema({
+    title: String,
+    message: String,
+    creator: String,
+    tags: [String],
+    selectedFile: String,
+    likeCount:{
+        type: Number,
+        default: 0
+    },
+    createdAt:{
+        type: Date,
+        default: new Date()
+    },
+});
+
+const PostMessage = mangoose.model('PostMessage', postSchema);
+
+export default PostMessage;
---
+++ server/routes/posts.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+// create router object from express
+const router = express.Router();
+
+import { getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
+
+router.get('/', getPosts);
+router.post('/', createPost);
+router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
+
+export default router;
